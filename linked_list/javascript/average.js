82a622c4f392552d85a495478a4659c8cb17c519
Md Rahaman <soikatesc@gmail.com>
2017-05-07 22:54:38 -0500

    added find average for a given linked list

--- linked_list/javascript/average.js
+++ linked_list/javascript/average.js
@@ -15,8 +15,6 @@ LinkedList.prototype.Front = function(){
 	return this.head.val
 }
 
-
-
 LinkedList.prototype.addFront = function(val){
 	//creating a new node
 	var new_node = new Node(val);
@@ -34,10 +32,35 @@ LinkedList.prototype.addFront = function(val){
 	return this;
 }
 
+LinkedList.prototype.Length = function(){
+	runner = this.head;
+	count = 0;
+
+	while(runner){
+		count++;
+		runner = runner.next;
+	}
+	return count;
+}
+
+LinkedList.prototype.average = function(){
+	var runner = this.head;
+	var sum = 0;
+	var avg = 0;
+
+	while(runner){
+		sum += runner.val;
+		runner = runner.next;
+	}
+	return sum/this.Length()
+	
+}
+
 var list = new LinkedList()
 //test case
 list.addFront(5);
 list.addFront(10);
 list.addFront(1);
-console.log(list)
-console.log(list.Front())
+console.log(list.average())
+
+

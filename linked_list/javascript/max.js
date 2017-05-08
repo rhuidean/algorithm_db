0ca16aa1c9102d17e46cb9145fb924d950e10aa7
Md Rahaman <soikatesc@gmail.com>
2017-05-07 22:42:25 -0500

    linked list find max

---
+++ linked_list/javascript/max.js
@@ -0,0 +1,54 @@
+
+function Node(val){
+	this.val = val;
+	this.next = null;
+}
+
+function LinkedList(){
+	this.head = null;
+}
+
+LinkedList.prototype.Front = function(){
+	if(this.head == null){
+		return null;
+	}
+	return this.head.val
+}
+
+LinkedList.prototype.addFront = function(val){
+	//creating a new node
+	var new_node = new Node(val);
+
+	//if the list is empty
+	if(this.head == null){
+		this.head = new_node;
+	}
+	//if the list is not empty
+	else{
+		new_node.next = this.head;
+		this.head = new_node;
+	}
+
+	return this;
+}
+
+LinkedList.prototype.max = function(){
+	runner = this.head;
+	max = this.head.val
+	while(runner){
+		if(max<runner.val){
+			max = runner.val;
+		}
+		runner = runner.next;
+	}
+	return max
+}
+
+var list = new LinkedList()
+//test case
+list.addFront(5);
+list.addFront(10);
+list.addFront(1);
+console.log(list.max())
+
+

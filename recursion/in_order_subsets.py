dcf8742ba690069a61f3067173cfce8fc9bf6371
Jason <rhuidean06@gmail.com>
2017-06-05 11:50:02 -0500

    add final string order subsets version

--- recursion/in_order_subsets.py
+++ recursion/in_order_subsets.py
@@ -1,23 +1,24 @@
 def string_order_subsets(string):
 	### need to build recursion module error class
-	string_subset_list=[]
-	
+		
 	if isinstance(string,str):
 		string=[string[1:],string[0]]
 		print string
 		return string_order_subsets(string)
 
-	elif len(string)==1:
-		string_subset_list.append(string[0])
-		return 1
-
-	else:
-		
-		string1=[string[0][1:],string[1]]
+	elif string[0]=='':
 		
 		print string
-		return string_order_subsets(string1)	
-	# return string_subset_list
+		return string[1]+" "
+
+	else:
+		string0=[string[0][1:],string[1]+string[0][0]]
+		string1=[string[0][1:],string[1]]
+		
+		# print string0
+		# print string1
+		return string_order_subsets(string0)+string_order_subsets(string1)	
+	
 
 print string_order_subsets("abcd")
 

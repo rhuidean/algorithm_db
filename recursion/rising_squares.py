dfcf2525f0d9ea5fccb9bb6dcba041429e435dd0
Jason <rhuidean06@gmail.com>
2017-06-05 18:18:52 -0500

    add initial rising squares algo

---
+++ recursion/rising_squares.py
@@ -0,0 +1,17 @@
+def rising_squares(num):
+	### need to build recursion module error class
+		
+	if isinstance(num,int):
+		print 1
+		num={'array':[1],'num':num}
+			return rising_squares(num)
+
+	elif len(num['array'])==num['num']:
+		last_index=len(num['array'])-1
+		print num['array'][last_index]+2
+			return rising_squares(num)
+
+	elif len(num['array'])<num['num']/2:
+			 print num['array'][]
+
+	else

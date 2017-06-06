675989129077546942d2f6c7545ecc5fc5c19a9e
Jason <rhuidean06@gmail.com>
2017-06-06 14:23:06 -0500

    add tower of hanoi initial algo

---
+++ recursion/towers_of_hanoi.py
@@ -0,0 +1,33 @@
+def towers_of_hanoi(num):
+	import math 
+		
+	### start memoization
+	if isinstance(num,int):
+		list_a = [number for number in reversed(range(1,num+1))]
+		list_b = []
+		list_c = []
+		num = {"list_a":list_a,"list_b":list_b,"list_c":list_c}
+		print num['list_a']
+		print num['list_b']
+		print num['list_c']
+		print "-"*30
+		return towers_of_hanoi(num)
+
+	elif len(num["list_c"])==0:
+		num["list_c"].append(num['list_a'].pop())
+		print num['list_a']
+		print num['list_b']
+		print num['list_c']
+		print "-"*30
+		return towers_of_hanoi(num)
+
+	elif len(num["list_b"])==0:
+		num["list_b"].append(num['list_a'].pop())
+		print num['list_a']
+		print num['list_b']
+		print num['list_c']
+		print "-"*30
+		return towers_of_hanoi(num)
+
+
+print towers_of_hanoi(9)

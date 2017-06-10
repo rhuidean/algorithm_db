06168f991c059e0c02aa560e426b488f4efa9ec0
Jason <rhuidean06@gmail.com>
2017-06-09 21:22:08 -0500

    add modified insert file

--- tries/tries.py
+++ tries/tries.py
@@ -3,16 +3,27 @@ class TrieNode(object):
 	### declare attributes
 	def __init__(self):
 		### by default node's value is an empty string
-		self.value = ""
-		self.children = []
+		self.key = None
+		self.value = None
+		self.children = {}
 
 
 ### Create Triset object 
 class Triset(object):
 	### declared above the __init__ becomes attribute
 	new_node=TrieNode()
+	new_node.key=""
+
 	def __init__(self):
 		self.root = self.new_node
 
-triset1=Triset()
-print "{} {}".format(triset1.root.value,triset1.root.children)
+# triset1=Triset()
+# print "{} {} {}".format(triset1.root.key,triset1.root.value,triset1.root.children)
+
+# triset1.root.key="a"
+# print triset1.root
+
+
+# string = "abc"
+
+# print len(string[3:])

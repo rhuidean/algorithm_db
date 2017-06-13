737d5d50731d62ddb15f31a54bc169d1e3d11324
Jason <rhuidean06@gmail.com>
2017-06-13 16:33:46 -0500

    add final graph class file

---
+++ trees/tree.py
@@ -0,0 +1,12 @@
+class BTNode(object):
+	def __init__(self,value):
+		self.val = value
+		self.left = None
+		self.right = None
+
+
+class BST(object):
+	def __init__(self):
+		self.root = None
+
+	
